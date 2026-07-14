beeec3058df4a30cbe8ed17b399b0c20f1bfba09
AntonDav23 <antonses@icloud.com>
2026-07-14 16:26:00 +0300

    'creation_mixin_class'

---
+++ src/product_mixin.py
@@ -0,0 +1,10 @@
+class CreationLoggerMixin:
+    """ Миксин для логирования создания объектов. Выводит в консоль имя класса и параметры инициализации """
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
+
+        print(f"Создан объект {self.__class__.__name__}({signature})")
